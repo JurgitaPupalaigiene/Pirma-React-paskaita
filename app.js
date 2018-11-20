8519d22db5883aabc9af9dbbcc21f1e4ae464344
Jurgita <jurgita.pupalaigiene@gmail.com>
2018-11-20 22:45:48 +0200

    pakeitimai del eksperimento

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 console.log('Pasileido')
-for (var i=0; i<10; i++){
-    console.log('Pasileido'+ i)
+for (var i = 0; i < 10; i++) {
+    console.log('Pasileido' + i)
 }
-const component=<h1>Hello, world</h1>;
+const component = < h1 > Hello,
+    world < /h1>;
 
 ReactDOM.render(
     component,
+    document.getElementById('root')
+);
+
+var HelloComponent = React.createClass({
+    render: function () {
+        return React.createElement(
+            "div",
+            null,
+            "Hello ",
+            this.props.name
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(
+        HelloComponent, {
+            name: "Jane"
+        }),
     document.getElementById('root')
 );
